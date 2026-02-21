0e0d4e9220ba03ec6a1a8aec6453c1728c7692ae
unknown <jerry_garcia_07@hotmail.com>
2026-02-21 16:36:43 -0500

    subir nuevos archivos

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,65 @@
-import './App.css'
-import { Route, Routes } from 'react-router';
-import Login from './pages/Login';
-import NotFound from './pages/NotFountPage';
-import Home from './pages/Home';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router';
-import { supabase } from './supabase/client';
-import { CategoriProvider } from './contexts/CategoriContext';
+import "./App.css";
+import { Route, Routes, Navigate } from "react-router";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFountPage";
+import Home from "./pages/Home";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import { supabase } from "./supabase/client";
+import { CategoriProvider } from "./contexts/CategoriContext";
+import Category from "./pages/Category";
+import CreateCategory from "./pages/CreateCategory";
 function App() {
+  const [session, setSession] = useState(null);
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
     supabase.auth.onAuthStateChange((event, session) => {
-      if (!session) {
-        navigate('/login');
-      } else {
-        navigate('/');
-      }
+      setSession(session);
+      setLoading(false);
     });
-  },[]);
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  if (loading) {
+    return <div>Cargando apliacacion </div>;
+  }
 
   return (
     <>
-    <CategoriProvider>
-      <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/login" element={<Login/>}></Route>
-        <Route path="*" element={<NotFound/>}></Route>
-      </Routes>
-    </CategoriProvider>
+      <CategoriProvider>
+        <Routes>
+          <Route
+            path="/login"
+            element={!session ? <Login /> : <Navigate to="/" replace />}
+          ></Route>
+          <Route
+            path="/"
+            element={session ? <Home /> : <Navigate to="/login" replace />}
+          ></Route>
+          <Route
+            path="/Categoria"
+            element={session ? <Category /> : <Navigate to="/login" replace />}
+          ></Route>
+          <Route
+            path="/CrearCategoria"
+            element={
+              session ? <CreateCategory /> : <Navigate to="/login" replace />
+            }
+          ></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </CategoriProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,41 +3,45 @@ import { supabase } from "../supabase/client";
 import { useNavigate } from "react-router";
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const navagation=useNavigate();
+  const [email, setEmail] = useState("");
+  const navagation = useNavigate();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        console.log(email);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        try {
-            const result = await supabase.auth.signInWithOtp({ email });
-            console.log(result);
-        } catch (error) {
-            console.log(error);
-        }
-    };
+    try {
+      const result = await supabase.auth.signInWithOtp({ email });
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    useEffect(()=>{
-        const checksession=async()=>{
-            const {data:{user}}=await supabase.auth.getUser();
-            console.log(user)
-            if(user){
-                navagation('/');
-            }
-        }
-        checksession();
-    },[])
+  // useEffect(()=>{
+  //     const checksession=async()=>{
+  //         const {data:{user}}=await supabase.auth.getUser();
+  //         console.log(user)
+  //         if(user){
+  //             navagation('/');
+  //         }
+  //     }
+  //     checksession();
+  // },[])
 
-    return (
-        <div>
-            <h1>Login</h1>
-            <form onSubmit={handleSubmit}>
-                <input type="email" name="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-                <button type="submit">Login</button>
-            </form>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
