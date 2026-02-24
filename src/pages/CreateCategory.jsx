9d295e8c27c47593552ab7aa1ce77603b074026c
unknown <jerry_garcia_07@hotmail.com>
2026-02-24 12:06:50 -0500

    lisado icon

--- src/pages/CreateCategory.jsx
+++ src/pages/CreateCategory.jsx
@@ -2,9 +2,13 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router";
 import { supabase } from "../supabase/client";
 import CategoriForm from "../components/CategoriForm";
+import * as GiIcons from "react-icons/gi";
 
 const CreateCategory = () => {
   const navagation = useNavigate();
+
+  const iconList = Object.entries(GiIcons);
+  console.log(iconList);
 
   useEffect(() => {
     const checksession = async () => {
@@ -22,6 +26,13 @@ const CreateCategory = () => {
       <h1>CreateCategory</h1>
       <button onClick={() => supabase.auth.signOut()}>Logout</button>
       <CategoriForm />
+      <div>
+        {iconList.slice(0, 50).map(([name, Icon]) => (
+          <button key={name} className="iconBox">
+            <Icon size={30} />
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
