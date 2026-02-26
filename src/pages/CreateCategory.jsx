b85a92d44c05e8cdb339b79c9b39342bc7deb55c
unknown <jerry_garcia_07@hotmail.com>
2026-02-26 00:05:56 -0500

    agregar botones

--- src/pages/CreateCategory.jsx
+++ src/pages/CreateCategory.jsx
@@ -1,37 +1,46 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import { supabase } from "../supabase/client";
 import CategoriForm from "../components/CategoriForm";
-import * as GiIcons from "react-icons/gi";
+import * as FaIcons from "react-icons/fa";
 
 const CreateCategory = () => {
+  const [nameIcon, setNameIcon] = useState(null);
+
   const navagation = useNavigate();
 
-  const iconList = Object.entries(GiIcons);
-  console.log(iconList);
+  const iconNames = Object.keys(FaIcons);
 
   useEffect(() => {
     const checksession = async () => {
       const {
         data: { user },
       } = await supabase.auth.getUser();
-      if (!user) {
-        navagation("/login");
-      }
+      // if (!user) {
+      //   navagation("/login");
+      // }
     };
     checksession();
   }, [navagation]);
+
+  useEffect(() => {
+    console.log(nameIcon);
+  }, [nameIcon]);
   return (
     <div>
       <h1>CreateCategory</h1>
       <button onClick={() => supabase.auth.signOut()}>Logout</button>
       <CategoriForm />
       <div>
-        {iconList.slice(0, 50).map(([name, Icon]) => (
-          <button key={name} className="iconBox">
-            <Icon size={30} />
-          </button>
-        ))}
+        {iconNames.map((iconName) => {
+          const IconCompoent = FaIcons[iconName];
+
+          return (
+            <button key={iconName} onClick={() => setNameIcon(iconName)}>
+              <IconCompoent size={32}></IconCompoent>
+            </button>
+          );
+        })}
       </div>
     </div>
   );
