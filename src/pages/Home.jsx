dd05a62ef31858f72f563c5be4423fae83fcb01d
unknown <jerry_garcia_07@hotmail.com>
2026-03-17 00:11:13 -0500

    condicion de visualizacion de home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,19 +4,29 @@ import { supabase } from "../supabase/client";
 import CategoriForm from "../components/CategoriForm";
 import CategoriList from "../components/CategoriList";
 import MoviList from "../components/MoviList";
+import { useCategoria } from "../contexts/CategoriContext";
 
 const Home = () => {
-  return (
-    <div>
-      <h1>¡Bienvenido a tu gestor de gastos!</h1>
-      <p>
-        Empieza a organizar tus finanzas de forma sencilla y mantén tus gastos
-        bajo control.
-      </p>
-      <button>Agregar</button>
-      <MoviList />
-    </div>
-  );
+  const { movimiento } = useCategoria();
+
+  const HomeVisit = () => {
+    if (movimiento.lenght === 0) {
+      return (
+        <div>
+          <h1>¡Bienvenido a tu gestor de gastos!</h1>
+          <p>
+            Empieza a organizar tus finanzas de forma sencilla y mantén tus
+            gastos bajo control.
+          </p>
+          <button>Agregar</button>
+        </div>
+      );
+    } else {
+      return <MoviList />;
+    }
+  };
+
+  return HomeVisit();
 };
 
 export default Home;
